8a68a519d94dcd5afe36a4739242a1deb25f3d2d
seeladelaxsniki-lgtm <seeladelaxsniki@gmail.com>
2026-05-20 18:29:41 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -2,12 +2,10 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-# главная страница
 @app.route("/")
 def home():
     return send_from_directory("static", "index.html")
 
-# статика (css/js если будут)
 @app.route("/<path:path>")
 def static_files(path):
     return send_from_directory("static", path)
