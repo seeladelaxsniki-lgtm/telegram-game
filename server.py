632c2c80d523b0b821ab0127be440b2044992b7c
seeladelaxsniki-lgtm <seeladelaxsniki@gmail.com>
2026-05-20 18:17:49 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -1,15 +1,18 @@
 from flask import Flask, send_from_directory
-import os
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__)
 
+# 🏠 ГЛАВНАЯ СТРАНИЦА
 @app.route("/")
 def home():
     return send_from_directory("static", "index.html")
 
+
+# (опционально) если есть файлы типа js/css
 @app.route("/<path:path>")
 def static_files(path):
     return send_from_directory("static", path)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
