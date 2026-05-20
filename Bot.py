7c8f7b7c1ba91a10b849fc7889ee09d0eabb44ff
seeladelaxsniki-lgtm <seeladelaxsniki@gmail.com>
2026-05-20 19:55:45 -0400

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -1,17 +1,25 @@
-import asyncio
-from aiogram import Bot, Dispatcher, types
+import requests
+import time
 
 TOKEN = "8749530424:AAGizeCaRJl1ReRkI-1j8Lefy0neB0Wh8o4"
+URL = f"https://api.telegram.org/bot{TOKEN}"
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
+last_update = 0
 
-@dp.message()
-async def start(msg: types.Message):
-    await msg.answer("🚀 Bot live")
+while True:
+    try:
+        r = requests.get(URL + f"/getUpdates?offset={last_update+1}").json()
 
-async def main():
-    await dp.start_polling(bot)
+        for result in r.get("result", []):
+            last_update = result["update_id"]
 
-if __name__ == "__main__":
-    asyncio.run(main())
+            chat_id = result["message"]["chat"]["id"]
+            text = result["message"].get("text", "")
+
+            if text == "/start":
+                requests.get(URL + f"/sendMessage?chat_id={chat_id}&text=Bot is alive 24/7 🚀")
+
+    except:
+        pass
+
+    time.sleep(2)
